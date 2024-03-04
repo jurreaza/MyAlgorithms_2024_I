6622e0b942798d5aa2db623e3255e07875466515
jurreaza <juanurrea322001@correo.itm.edu.co>
2024-03-03 19:05:52 -0500

    MyfifthAlgorithm_Conditionals_Even_and_Odd

---
+++ 06. MyFifthAlgorithm_Conditionals_Even_and_Odd/Program.cs
@@ -0,0 +1,30 @@
+﻿// Construir un programa que pida un número y luego diga si este número es par o no. 
+
+// Declarar Variables
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double N;
+        string M = "El número ingresado es: ";
+
+        // Pedir los datos
+        Console.WriteLine("Ingrese solamente números positivos \n");
+        Console.WriteLine("Ingrese el número:");
+        N = Convert.ToDouble(Console.ReadLine());
+
+        // Operación
+
+        if (N % 2 == 0)
+        {
+            Console.WriteLine($"{M} Par. ");
+        }
+        else
+        {
+            Console.WriteLine($"{M} Impar.");
+        }
+    }
+}
+
+

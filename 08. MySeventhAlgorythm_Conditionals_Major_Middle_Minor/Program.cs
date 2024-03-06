a0b1ca86b232045b72f22486d499c9bb6a2e8789
jurreaza <juanurrea322001@correo.itm.edu.co>
2024-03-05 19:44:11 -0500

    08. MySeventhAlgorythm_Conditionals_Major_Middle_Minor

---
+++ 08. MySeventhAlgorythm_Conditionals_Major_Middle_Minor/Program.cs
@@ -0,0 +1,39 @@
+﻿         //Construir un programa que pida por pantalla 3 números y luego diga cuál es el mayor, el del medio y el menor de los números ingresados.
+
+        //Declaración de Variables
+
+        double n1, n2, n3;
+
+        //Pedir datos al usuario
+
+        Console.WriteLine("Ingrese el primer número: ");
+        n1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Ingrese el segundo número: ");
+        n2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Ingrese el tercer número: ");
+        n3 = Convert.ToDouble(Console.ReadLine());
+
+        //Validacion de datos
+
+        if((n1 > n2) && (n2 > n3))
+        {
+            Console.WriteLine("El numero mayor es:" + n1);
+            Console.WriteLine("El numero medio es:" + n2);
+            Console.WriteLine("El numero menor es:" + n3);
+        }
+        else if ((n2 > n1) && (n1 > n3))
+        {
+            Console.WriteLine("El numero mayor es: " + n2);
+            Console.WriteLine("El numero medio es: " + n1);
+            Console.WriteLine("El numero menor es: " + n3);
+        }
+        else if ((n3 > n2) && (n2 > n1))
+        {
+            Console.WriteLine("El numero mayor es: " + n3);
+            Console.WriteLine("El numero medio es: " + n2);
+            Console.WriteLine("El numero menor es: " + n1);
+        }
+        else
+        {
+            Console.WriteLine("Hay numeros que son iguales.");
+        }

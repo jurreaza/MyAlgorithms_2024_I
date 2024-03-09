2697b7379889af14d7fde54cf2cc895782b0b20f
jurreaza <juanurrea322001@correo.itm.edu.co>
2024-03-08 23:33:29 -0500

    2. Point

--- 12. Exam/Program.cs
+++ 12. Exam/Program.cs
@@ -6,20 +6,20 @@
 
 //Declaración de primeras Variables
 
-int VTren1 = 80;  // en km/h
-int VTren2 = 100; // en km/h
-int HSTren1 = 10 * 60;  // 10:00 AM en minutos
-int HSTren2 = 11 * 60;  // 11:00 AM en minutos
+double VelTren1 = 80;  // en km/h
+double VelTren2 = 100; // en km/h
+double HSalTren1 = 10 * 60;  // 10:00 AM en minutos
+double HSalTren2 = 11 * 60;  // 11:00 AM en minutos
 
 // Calcular la distancia recorrida por el primer tren durante la hora de ventaja
-int TVentaja = HSTren2 - HSTren1;
-double DTren1 = VTren1 * (TVentaja / 60.0); // Convertir minutos a horas
+double TVentaja = HSalTren2 - HSalTren1;
+double DTren1 = VTren1 * (TVentaja / 60); // Convertir minutos a horas
 
 // Calcular el tiempo necesario para que el segundo tren alcance al primer tren
-double TNecesario = DTren1 / (VTren2 - VTren1);
+double TNecesario = DTren1 / (VelTren2 - VelTren1);
 
 // Calcular la hora en la que el segundo tren alcanzará al primer tren
-int HAlcance = HSTren2 + (int)(TNecesario * 60); // Convertir horas a minutos
+double HAlcance = HSalTren2 + (TNecesario * 60); // Convertir horas a minutos
 
 // Mostrar la hora en pantalla
 Console.WriteLine("El segundo tren alcanzará al primer tren a las: " + TimeSpan.FromMinutes(HAlcance).ToString("hh:mm"));
--- 13. Exam1/Program.cs
+++ 13. Exam1/Program.cs
@@ -1,40 +1,80 @@
-﻿
-using System;
-
-class Program
+﻿internal class Program
 {
-    static void Main()
+    private static void Main(string[] args)
     {
-
-    // Un tren sale de la estación “A” a las 10:00 AM y viaja a una velocidad constante de 80 km/h. 
-    //Un segundo tren sale de la misma estación a las 11:00 AM y viaja en la misma dirección a una velocidad 
-    // constante de 100 km/h. ¿A qué hora el segundo tren alcanzará al primer tren? 
-    //Mostrar esta hora en pantalla 
-
-    //Declaración de primeras Variables
-
-        int VTren1 = 80, VTren2 = 100;  // en km/h
-        int HSTren1, HSTren2;  // 10:00 AM & 11:00 AM en minutos
-        int HAlcance;
-        double Dtren1, TVentaja, TNecesario;
-
-        // Calcular la distancia recorrida por el primer tren durante la hora de ventaja
-
-        HSTren1 = 10 * 60;
-        HSTren2 = 11 * 60;
-        TVentaja = HSTren2 - HSTren1;
-        DTren1 = VTren1 * (TVentaja / 60.0); // Convertir minutos a horas
-
-        // Calcular el tiempo necesario para que el segundo tren alcance al primer tren
-
-        TNecesario = DTren1 / (VTren2 - VTren1);
-
-        // Calcular la hora en la que el segundo tren alcanzará al primer 
         
-        HAlcance = HSTren2 + (TNecesario * 60); // Convertir horas a minutos
+        // Realizar un algoritmo con C# donde se solucione el siguiente problema: Un vendedor recibe un 
+        // sueldo base de $XXXX. Además, recibe una comisión del 10% sobre el valor total de cada venta que 
+        // realiza. El vendedor desea saber: 
 
-        // Mostrar la hora en pantalla
-        
-        Console.WriteLine($"El segundo tren alcanzará al primer tren a las: {HAlcance} ");
+        // ¿Cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes? 
+        // ¿Cuál es el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones? 
+        // ¿Cuál fue la venta que le generó la mayor comisión? 
+        // ¿Cuál fue el promedio de las comisiones que recibió por cada venta?
+
+        // Además, la empresa da un beneficio extra de $100.000 si el vendedor supera el objetivo del mes. 
+        // El objetivo es sumar en las 3 ventas como mínimo $1.000.000 en ventas. 
+        // Mostrar un mensaje donde indique si ganó o no el beneficio.
+
+        //Declaración de Variables
+
+        double Sueldo, Venta1, Venta2, Venta3, VentaT;
+        double Comision1, Comision2, Comision3;
+        double ComisionP, ComisionM, STotal; 
+
+        //Solicitar Valores
+
+        Console.WriteLine("Ingrese el valor de su sueldo:");
+        Sueldo = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el valor de la primera venta:");
+        Venta1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el valor de la segunda venta:");
+        Venta2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el valor de la tercera venta:");
+        Venta3 = Convert.ToDouble(Console.ReadLine());
+
+        //Total que recibirá por comisión de Venta 10%
+
+        Comision1 = Venta1 * 0.10;
+
+        Comision2 = Venta2 * 0.10;
+
+        Comision3 = Venta3 * 0.10;
+
+        // Venta que le generará mayor comisión, su promedio & sueldo total.
+
+        ComisionP = (Comision1 + Comision2 + Comision3) / 3;
+        ComisionM = Comision1 + Comision2 + Comision3;
+        STotal = ComisionM + Sueldo;
+
+        if ((Comision1 > Comision2) & (Comision2 > Comision3))
+        {
+            Console.WriteLine($"La venta que generó mayor comisión fue la venta de: {Venta1}, el promedio de su comision fue de {ComisionP} & su sueldo total es {STotal}");
+        }
+        else if ((Comision2 > Comision1) & (Comision1 > Comision3))
+        {
+            Console.WriteLine($"La venta que generó mayor comisión fue la venta de: {Venta2}, el promedio de su comision fue de {ComisionP} & su sueldo total es {STotal}");
+        }
+        else
+        {
+            Console.WriteLine($"La venta que generó mayor comisión fue la venta de: {Venta3}, el promedio de su comision fue de {ComisionP} & su sueldo total es {STotal}");
+        }
+
+        // Validación de Bonificación por Ventas & Total a Pagar
+
+        VentaT = Venta1 + Venta2 + Venta3;
+
+        if(VentaT >= 1000000)
+        {
+            Console.WriteLine("Además, usted mi estimado vendedor ganó el beneficio extra");
+            
+        }
+        else
+        {
+            Console.WriteLine("Además, usted mi estimado vendedor no ganó el beneficio extra, pero en la próxima lo ganarás");
+        }
     }
 }
